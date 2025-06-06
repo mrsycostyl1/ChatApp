77c1f4432317291c0d3ebda70ff554dbb899d300
mrsycostyl1 <usertextnow11@gmail.com>
2025-06-06 11:02:18 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        neon: '#00f0ff',
+      },
+      animation: {
+        'spin-slow': 'spin 10s linear infinite',
+      },
+    },
+  },
+};
