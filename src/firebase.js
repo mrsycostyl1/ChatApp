3dacdf2ce3767a0494b54de512077e057a881f8f
mrsycostyl1 <usertextnow11@gmail.com>
2025-06-06 11:15:31 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "your-app.firebaseapp.com",
-  projectId: "your-app-id",
-  storageBucket: "your-app.appspot.com",
-  messagingSenderId: "xxxxxxx",
-  appId: "xxxxxxx"
+  apiKey: "AIzaSyB1Y3v-ui6PtDh94zGpO3EmcciJPRITPro",
+  authDomain: "my-air-899a7.firebaseapp.com",
+  databaseURL: "https://my-air-899a7-default-rtdb.firebaseio.com",
+  projectId: "my-air-899a7",
+  storageBucket: "my-air-899a7.appspot.com",
+  messagingSenderId: "739001861292",
+  appId: "1:739001861292:web:0b885a5269b67eb1f88b09"
 };
 
 const app = initializeApp(firebaseConfig);
